e02843bbec56ea40d36e1d5c302ff294b45cb586
Andruixxd31 <andruix93@gmail.com>
2020-01-20 23:50:16 -0600

    added a jar file to execute future programs and added and analysis of algorithm files to test the efficiency of some algorithms

--- binary_search/BinarySearch.java
+++ binary_search/BinarySearch.java
@@ -27,7 +27,7 @@ public class BinarySearch {
     }
     public static void main(String[] args) {
         int arr[] = {10,20,30,40,50,60,70,80,90};
-        int key = 30; 
+        int key = 33; 
         int result = binarySearch(arr,key);
         System.out.println("Postion of the key: " + result);
         System.out.println(1 / 2);
