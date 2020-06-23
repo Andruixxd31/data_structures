7c36efad125dd33628c94bd989f4792ebf991bcc
Andruixxd31 <andruix93@gmail.com>
2020-06-23 11:31:56 -0500

    made small asthetic changes to the files

--- linked_list/EmployeeLinkedList.java
+++ linked_list/EmployeeLinkedList.java
@@ -1,42 +1,43 @@
-
 public class EmployeeLinkedList {
 
-    private EmployeeNode head; //The start of the linked list. Is the last introduced element
+    private EmployeeNode head; // The start of the linked list. Is the last introduced element
     private int size;
 
     public int getSize() {
-        return this.size; //instead of iterating we get this variable to know the size
+        return this.size; // instead of iterating we get this variable to know the size
     }
 
     public boolean isEmpty() {
-        return this.head == null; //If there are no elements the head is null, it is started like that by default
+        return this.head == null; // If there are no elements the head is null, it is started like that by default
     }
 
-    public void addToFront(Employee employee){
-        EmployeeNode node = new EmployeeNode(employee); //creates the node of the employee added
-        node.setNext(head); //points to the node infront of it. the head is the element that came before this one
-        head = node; //the head is updated to this current employee, if anothe element is added this head will be used to point into this employee
-        size++; //each time an element is added this will be increased
+    public void addToFront(Employee employee) {
+        EmployeeNode node = new EmployeeNode(employee); // creates the node of the employee added
+        node.setNext(head); // points to the node infront of it. the head is the element that came before
+                            // this one
+        head = node; // the head is updated to this current employee, if anothe element is added this
+                     // head will be used to point into this employee
+        size++; // each time an element is added this will be increased
     }
 
     public EmployeeNode removeFromFront() {
-        if(isEmpty()){  //wont do anything if there are no employees
+        if (isEmpty()) { // wont do anything if there are no employees
             return null;
         }
-        EmployeeNode removedNode = head; //will be used to set this node to null
-        head = head.getNext(); //head is now the element infront of it, sinces this one will be deleted
-        size--; //decreasing size to keep count of node
-        removedNode.setNext(null); //making null
+        EmployeeNode removedNode = head; // will be used to set this node to null
+        head = head.getNext(); // head is now the element infront of it, since this one will be deleted
+        size--; // decreasing size to keep count of node
+        removedNode.setNext(null); // making null
         return removedNode;
     }
 
     public void printList() {
-        EmployeeNode current = head; //used to traverse the linked list
+        EmployeeNode current = head; // used to traverse the linked list
         System.out.println("HEAD ->");
-        while(current != null){
+        while (current != null) {
             System.out.print(current);
             System.out.println(" ->");
-            current = current.getNext(); 
+            current = current.getNext();
         }
         System.out.println("null");
     }
--- linked_list/EmployeeNode.java
+++ linked_list/EmployeeNode.java
@@ -1,30 +1,29 @@
-
 public class EmployeeNode {
 
     private Employee employee;
-    private EmployeeNode next; //Node that will be used to point to node infront of it
+    private EmployeeNode next; // Node that will be used to point to node infront of it
 
-    public EmployeeNode(Employee employee){
+    public EmployeeNode(Employee employee) {
         this.employee = employee;
     }
 
-    public Employee getEmployee(){
+    public Employee getEmployee() {
         return this.employee;
     }
 
-    public void setEmployee(Employee employee){
+    public void setEmployee(Employee employee) {
         this.employee = employee;
     }
 
-    public EmployeeNode getNext(){
-        return this.next; 
+    public EmployeeNode getNext() {
+        return this.next;
     }
 
-    public void setNext(EmployeeNode next){
-        this.next = next; //wiil be used to point to the next node
+    public void setNext(EmployeeNode next) {
+        this.next = next; // wiil be used to point to the next node
     }
 
-    public String toString(){
+    public String toString() {
         return employee.toString();
     }
 
